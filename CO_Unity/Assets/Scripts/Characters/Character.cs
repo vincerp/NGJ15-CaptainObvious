be1e7906e67ba88786f038b43ea3a335ecf92bc0
Victor Apihtin <vapyhtin@gmail.com>
2015-02-07 02:52:47 +0100

    * holster * speech bubble * characters code starting up

---
+++ CO_Unity/Assets/Scripts/Characters/Character.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour 
+{
+	public float speechTime = 1f;
+
+
+	private bool _isDead = false;
+	private SpeechBubble _sBubble = null;
+
+	
+	private Animator _animator;
+	
+
+	public void Speak( string speech )
+	{
+		_sBubble.Activate( speech );
+		_sBubble.Invoke( "Deactivate", speechTime );
+	}
+
+	public void Die()
+	{
+		_isDead = true;
+		_animator.SetTrigger("event_Died");
+	}
+
+	void Awake()
+	{
+		_animator = GetComponent<Animator>();
+		_sBubble = GetComponentInChildren<SpeechBubble>();
+		if( _sBubble == null )
+		{
+			GameObject speechGO = GameObject.Instantiate(Resources.Load("Characters/SpeechBubble")) as GameObject;
+			_sBubble = speechGO.GetComponent<SpeechBubble>();
+		}
+	}
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+
+}
---
+++ CO_Unity/Assets/Scripts/Objects/Holster.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Holster : InteractiveObject {
+
+	private LineRenderer _lineRenderer;
+
+	void Awake()
+	{
+		_lineRenderer = transform.GetComponent<LineRenderer>();
+		_lineRenderer.SetVertexCount(transform.parent.childCount);
+	}
+
+	void Update()
+	{
+		for (int i = 0; i < transform.parent.childCount; i++ )
+			_lineRenderer.SetPosition( i, transform.parent.GetChild(i).position );
+
+	}
+
+	void OnTriggerEnter2D( Collider2D collider )
+	{
+		base.OnTriggerEnter2D(collider);
+		Debug.Log("Holster: on trigger enter");
+	}
+}
