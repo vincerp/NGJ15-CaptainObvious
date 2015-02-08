6971926c436fcf4a836c7c99152838055779e875
cristi16 <patrascu.cristinel@gmail.com>
2015-02-08 01:10:47 +0100

    punchable hurdle prefab

---
+++ CO_Unity/Assets/Scripts/Objects/Stand.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stand : MonoBehaviour
+{
+    public float fallForce = 5f;
+    private Rigidbody2D light;
+    private Transform puncher;
+    private Rigidbody2D border;
+
+    void Start()
+    {
+        light = transform.GetChild(0).GetComponent<Rigidbody2D>();
+        puncher = GameObject.FindGameObjectWithTag("Player").transform.FindChild("PunchArea");
+        border = GetComponent<Rigidbody2D>();
+    }
+
+    public void TipOver()
+    {
+        if(light.transform.parent != null)
+        {
+            light.transform.SetParent(null);
+            light.AddRelativeForce((light.transform.position - puncher.position).normalized * fallForce, ForceMode2D.Impulse);
+        }
+        border.AddRelativeForce((light.transform.position - puncher.position).normalized * fallForce, ForceMode2D.Impulse);
+    }
+
+}
