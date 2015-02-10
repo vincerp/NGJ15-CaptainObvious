ba6d99e2b508b84481722f62b92177a03ddad7e0
vincerp <vincependrell@gmail.com>
2015-02-10 11:37:09 +0100

    • Cinematic → Game music transition

--- CO_Unity/Assets/Scripts/InfiniteMusic.cs
+++ CO_Unity/Assets/Scripts/InfiniteMusic.cs
@@ -1,10 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 [RequireComponent(typeof(AudioSource))]
 public class InfiniteMusic : MonoBehaviour {
 
+	[Range(0f, 1f)][SerializeField]float cinematicMusic, gameMusic;
+	[SerializeField]int[] levelsWithCinematicMusic;
+	bool isCinematic = true;
+
+
 	void Start () {
 		DontDestroyOnLoad(gameObject);
+	}
+
+	void OnLevelWasLoaded(int lvl){
+		bool tryCinematic = levelsWithCinematicMusic.Contains(lvl);
+
+		if(tryCinematic != isCinematic)
+			iTween.AudioTo(gameObject, ((isCinematic)?cinematicMusic:gameMusic), 1f, 0.25f);
 	}
 }
